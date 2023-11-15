6f90822aa2be707d2e4290f68488f5d9d75b582a
Dan <daniel.speranskiy@live.com>
2023-11-15 18:27:33 +0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ weight:['200', '400', '600', '700', '800'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }

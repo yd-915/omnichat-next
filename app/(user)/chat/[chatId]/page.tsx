5b418e66818ad68e17c2b7ee1d65d974adcca85d
Dan <daniel.speranskiy@live.com>
2023-11-17 12:03:34 +0400

    rendering translated messages on individual chats

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,6 +1,10 @@
 import { authOptions } from '@/auth'
 import ChatInput from '@/components/ChatInput'
+import ChatMessages from '@/components/ChatMessages'
+import { sortedMessagesRef } from '@/lib/converters/Message'
+import { getDocs } from 'firebase/firestore'
 import { getServerSession } from 'next-auth'
+import { init } from 'next/dist/compiled/webpack/webpack'
 import React from 'react'
 
 type Props = {
@@ -12,13 +16,20 @@ type Props = {
 async function ChatPage({ params: { chatId }}:Props) {
   const session = await getServerSession(authOptions)
 
+  const initialMessages = (await getDocs(sortedMessagesRef(chatId))).docs.map((doc)=> doc.data())
+
   return (
     <>
   {/* Admin control */}
 
   {/* Chats member badge */}
 
-  {/* Chat messages */}
+  <div className='flex-1'>
+    <ChatMessages chatId={chatId}
+    session={session}
+    initialMessages={initialMessages}
+    />
+  </div>
 
 
   <ChatInput chatId={chatId}/>

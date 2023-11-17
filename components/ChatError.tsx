43168d057749bcd48e8cf611ffe5fb21114f11b2
Dan <daniel.speranskiy@live.com>
2023-11-17 20:03:57 +0400

    Added an alert for wrong pages

--- components/ChatError.tsx
+++ components/ChatError.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { AlertCircle } from "lucide-react"
+import {Alert, AlertDescription, AlertTitle} from './ui/alert'
+import { Button } from "./ui/button"
+import Link from "next/link"
 
 function ChatError() {
   return (
-    <div>ChatError</div>
+    <Alert variant="destructive">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription className="flex-1">
+        <p className="flex-1">You don't have permission to access this page
+        <br />
+        <span className="font-bold">
+        Please ask the chat Administrator to add you in the chat.
+        </span>
+        </p>
+        <Link href='/chat' replace>
+        <Button variant="destructive">Dismiss</Button>
+        </Link>
+      </AlertDescription>
+    </Alert>
   )
 }
 

e645c2c277654830f1b2051a16d72fe3632f9957
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-01-04 16:45:32 -0500

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,14 @@ import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDIYgPpMadE3sne0MC3kHTxajVHNo3XYtM",
-    authDomain: "omnichat-b1429.firebaseapp.com",
-    projectId: "omnichat-b1429",
-    storageBucket: "omnichat-b1429.appspot.com",
-    messagingSenderId: "424712602946",
-    appId: "1:424712602946:web:43a4c0952c9e94a611f4db"
+      apiKey: "AIzaSyAt6GaS5rZNxXLSaNf7QsDzR14akBQKNcU",
+    authDomain: "ganchat-dd078.firebaseapp.com",
+    projectId: "ganchat-dd078",
+    storageBucket: "ganchat-dd078.appspot.com",
+    messagingSenderId: "728824725279",
+    appId: "1:728824725279:web:4a49173d576f72e59aba2a",
+    measurementId: "G-TBX6YY8S41"
+
   };
 
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
